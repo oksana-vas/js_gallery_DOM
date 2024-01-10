ade6bde9ac71b7e92ac510eb56bb1f6c4050c0de
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2024-01-10 15:08:01 +0100

    make build

---
+++ index.b1ba44b0.js
@@ -0,0 +1,4 @@
+var e=document.getElementById("largeImg");// eslint-disable-next-line no-shadow
+document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var l=t.target.closest(".list-item__link");e.src=l.href});//# sourceMappingURL=index.b1ba44b0.js.map
+
+//# sourceMappingURL=index.b1ba44b0.js.map
